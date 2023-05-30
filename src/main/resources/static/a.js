cced7e9ac758b3ae02fee95d6747c3dfebb3f797
Furkan Kiraz <furkankirazmp@gmail.com>
2023-05-31 00:54:01 +0300

    sudoku solver added

--- src/main/resources/static/a.js
+++ src/main/resources/static/a.js
@@ -210,7 +210,7 @@ function setValues() {
 function refreshNav(algorithm) {
     switch (algorithm) {
         case "backtracking":
-            solveSudoku(true);
+            solveSudoku1();
             break;
         case "bee":
             solveSudoku(true);
@@ -257,6 +257,77 @@ function solveWithOptions() {
     updateRemainingTable();
     ViewPuzzleByGrid(answer);
      */
+}
+
+function solveSudoku1() {
+    // read current state
+    puzzle = readInput();
+
+    var columns = getColumns(puzzle);
+    var blocks = getBlocks(puzzle);
+
+    // check if there is any conflict
+    var errors = 0;
+    var correct = 0;
+
+    for (var i = 0; i < puzzle.length; i++) {
+        for (var j = 0; j < puzzle[i].length; j++) {
+            var result = checkValue(
+                puzzle[i][j],
+                puzzle[i],
+                columns[j],
+                blocks[Math.floor(i / 3) * 3 + Math.floor(j / 3)],
+                -1,
+                -1
+            );
+            correct = correct + (result === 2 ? 1 : 0);
+            errors = errors + (result > 2 ? 1 : 0);
+            addClassToCell(
+                table.rows[i].cells[j].getElementsByTagName("input")[0],
+                result > 2 ? "wrong-cell" : undefined
+            );
+        }
+    }
+
+    // check if invalid input
+    if (errors > 0) {
+        canSolved = false;
+        return 2;
+    }
+
+    canSolved = true;
+    isSolved = true;
+
+    // check if grid is already solved
+    if (correct === 81) {
+        return 1;
+    }
+
+    //read the current time
+    var time = Date.now();
+
+    // solve the grid
+    solution = solveSudoku2(convertGrid(puzzle));
+    // show result
+    // get time
+    time = Date.now() - time;
+
+    if (true)
+        document.getElementById("timer").innerText =
+            Math.floor(time / 1000) + "." + ("000" + (time % 1000)).slice(-3);
+
+    if (solution === undefined) {
+        isSolved = false;
+        canSolved = false;
+        return 3;
+    }
+
+    if (true) {
+        remaining = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        updateRemainingTable();
+        ViewPuzzle(solution);
+    }
+    return 0;
 }
 
 // level is current row number in the grid
@@ -307,7 +378,6 @@ function generatePossibleRows(possibleNumber) {
             result.push(PossibleRow);
             return;
         }
-
         for (var i in possibleNumber[level]) {
             if (PossibleRow.includes(possibleNumber[level][i])) continue;
             step(level + 1, PossibleRow + possibleNumber[level][i]);
@@ -398,6 +468,8 @@ function readInput() {
     }
     return result;
 }
+
+
 
 // check value if it is correct or wrong
 // return:
@@ -1089,6 +1161,7 @@ function isUniqueButtonClick() {
 
 function gridSettings(rowCount, columnCount) {
     var table = document.getElementById("puzzle-grid");
+    var index = 1;
     if (rowCount === 16) document.getElementById("game-status").style.width = "200px";
     else document.getElementById("game-status").style.width = "inherit";
     this.rowCount = rowCount;
@@ -1110,16 +1183,332 @@ function gridSettings(rowCount, columnCount) {
                 cell1.innerHTML = "<input type=\"text\" maxlength=\"1\" onchange=\"checkInput(this)\" disabled />";
             }
             else if (rowCount==9) {
+                cell1.innerHTML= '<input class="js-field" maxLength="1" type="text" data-index="' + (index) +'" data-row="' + i +'" data-col="' + j +'"/>';
                 cell1.classList.add("td-3");
-                cell1.innerHTML = "<input type=\"text\" maxlength=\"1\" onchange=\"checkInput(this)\" disabled />";
+                index++;
+                //cell1.innerHTML = "<input type=\"text\" maxlength=\"1\" onchange=\"checkInput(this)\" disabled />";
             }
             else if (rowCount == 16) {
                 cell1.classList.add("td-4");
-                cell1.innerHTML = "<input type=\"text\" maxlength=\"2\" onchange=\"checkInput(this)\" disabled />";
+                cell1.innerHTML= '<input class="js-field" maxLength="2" type="text" data-index="' + (index) +'" data-row="' + i +'" data-col="' + j +'"/>';
+                index++;
             }
 
         }
     }
     mainFunction();
     sudokuSolverMenuClick();
+    var so = new Sudoku(document.getElementById("sudoku"));
+}
+
+class Sudoku {
+    constructor(container, { controls = true, pauseOnGuess = false } = {}) {
+        this.container = document.getElementById("puzzle-grid");
+        var controlContainer = document.getElementById("controls");
+        this._pauseOnGuess = pauseOnGuess;
+        this.init(controls);
+        if (controls) {
+            controlContainer.querySelector(".js-solve").addEventListener("click", ev => this.solve());
+            controlContainer.querySelector(".js-play").addEventListener("click", ev => this.stepSolve());
+            controlContainer.querySelector(".js-pause").addEventListener("click", ev => this.pause());
+            controlContainer.querySelector(".js-continue").addEventListener("click", ev => this.continue());
+            controlContainer.querySelector(".js-reset").addEventListener("click", ev => this.reset());
+            controlContainer.querySelector(".js-clear").addEventListener("click", ev => this.clearBoard());
+        }
+    }
+
+    init(controls = true) {
+        let container = this.container;
+        let fields = container.querySelectorAll("input.js-field");
+        fields.forEach(input => {
+            input.addEventListener("click", ev => {
+                input.focus();
+                input.select();
+            });
+            input.addEventListener("focus", ev => {
+                let index = +input.dataset.index;
+                log(container, `Allowed digits:
+      ${b2ds(analyze(this.readBoard()).allowed[index]).join(", ")}`);
+            });
+            input.addEventListener("keydown", ev => {
+                let idx = +input.dataset.index;
+                if (ev.keyCode >= 48 && ev.keyCode <= 57) {
+                    input.value = String.fromCharCode(ev.keyCode);
+                    if (input.value == "0") input.value = "";
+                    input.select();
+                    ev.preventDefault();
+                } else {
+                    let parent = input.parentNode;
+                    let next;
+                    switch (ev.keyCode) {
+                        case 38: // ↑
+                            next = idx - 9; break;
+                        case 40: // ↓
+                            next = idx + 9; break;
+                        case 39: // →
+                            next = idx + 1; break;
+                        case 37: // ←
+                            next = idx - 1; break;
+                    }
+                    if (next != null) {
+                        if (next < 0) next += 81;
+                        next %= 81;
+                        next = parent.querySelector(`input[data-index="${next}"]`);
+                        next.focus();
+                        next.select();
+                        ev.preventDefault();
+                    }
+                }
+            });
+        });
+    }
+
+    pause() {
+        clearTimeout(this._playTimer);
+        this.container.classList.add("paused");
+    }
+
+    continue() {
+        this.container.classList.remove("paused");
+        this._playCont();
+    }
+
+    readBoard(init = false) {
+        return [...this.container.querySelectorAll("input.js-field")]
+            .map(el => {
+                if (init) el.classList.toggle("init", el.value);
+                return el.value ? d2b(parseInt(el.value, 10)) : 0;
+            });
+    }
+
+    writeBoard(values, init = false) {
+        let el = this.container;
+        [...el.querySelectorAll("input.js-field")].forEach((el, i) => {
+            el.value = values[i] || "";
+            el.classList.remove("current");
+            if (init) {
+                el.classList.toggle("init", values[i]);
+            }
+        });
+        if (init) {
+            this._initBoard = values;
+            this._reset();
+        }
+    }
+
+    writeBytes(values, init = false) {
+        this.writeBoard(values.map(b2d), init);
+    }
+
+    clearBoard() {
+        let el = this.container;
+        [...el.querySelectorAll("input.js-field")].forEach(el => {
+            el.value = "";
+            el.classList.remove("init");
+        });
+        this._reset();
+    }
+
+    reset() {
+        this.writeBoard(this._initBoard || [], true);
+    }
+
+    _reset() {
+        let el = this.container;
+        clearTimeout(this._playTimer);
+        el.classList.remove("solved", "playing", "paused");
+        log(el, "");
+    }
+
+    solve() {
+        let self = this;
+        let el = self.container;
+        let board = self.readBoard(true);
+        let backtrack = 0;
+        let guesswork = 0;
+        let dcount = 0;
+        let time = Date.now();
+        if (solve()) {
+            stats();
+            self.writeBytes(board);
+            el.classList.add("solved");
+        } else {
+            stats();
+            alert("no solution");
+        }
+        function solve() {
+            let { index, moves, len } = analyze(board);
+            if (index == null) return true;
+            if (len > 1) guesswork++;
+            for (let m = 1; moves; m <<= 1) {
+                if (moves & m) {
+                    dcount++;
+                    board[index] = m;
+                    if (solve()) return true;
+                    moves ^= m;
+                }
+            }
+            board[index] = 0;
+            ++backtrack;
+            return false;
+        }
+        function stats() {
+            log(el, `${dcount} digits placed<br>${backtrack} take-backs<br>${guesswork} guesses<br>${Date.now() - time} milliseconds`);
+        }
+    }
+
+    stepSolve() {
+        let self = this;
+        let el = self.container;
+        el.classList.add("playing");
+        let board = self.readBoard(true);
+        let backtrack = 0;
+        let guesswork = 0;
+        let dcount = 0;
+        solve(success => {
+            log(el, `${backtrack} take-backs<br>${guesswork} guesses`);
+            el.classList.remove("playing");
+            if (success) {
+                self.writeBytes(board);
+                el.classList.add("solved");
+            } else {
+                alert("no solution");
+            }
+        });
+        function solve(cb) {
+            let { index, moves, len } = analyze(board);
+            if (index == null) return cb(true);
+            if (self._pauseOnGuess) {
+                el.querySelector('input[data-index="' + index + '"]').classList.add("current");
+            }
+            if (len > 1) {
+                guesswork++;
+                if (self._pauseOnGuess) {
+                    self._playCont = () => loop(moves, 1);
+                    stats();
+                    self.pause();
+                    return;
+                }
+            }
+            function loop(moves, m){
+                if (!moves) {
+                    board[index] = 0;
+                    ++backtrack;
+                    stats();
+                    cb(false);
+                } else if (moves & m) {
+                    dcount++;
+                    stats();
+                    board[index] = m;
+                    self.writeBytes(board);
+                    el.querySelector('input[data-index="' + index + '"]').classList.add("current");
+                    self._playTimer = setTimeout(self._playCont = () =>
+                        solve(success => success ? cb(true) : loop(moves ^ m, m << 1)), 100);
+                } else loop(moves, m << 1);
+            };
+            loop(moves, 1);
+        }
+        function stats() {
+            log(el, `${dcount} digits placed<br>${backtrack} take-backs<br>${guesswork} guesses`);
+        }
+    }
+}
+
+function i2rc(index) {
+    return { row: index / 9 | 0, col: index % 9 };
+}
+
+function rc2i(row, col) {
+    return row * 9 + col;
+}
+
+function d2b(digit) {
+    return 1 << (digit - 1);
+}
+
+function b2d(byte) {
+    for (var i = 0; byte; byte >>= 1, i++);
+    return i;
+}
+
+function b2ds(byte) {
+    let digits = [];
+    for (let i = 1; byte; byte >>= 1, i++)
+        if (byte & 1) digits.push(i);
+    return digits;
+}
+
+function log(el, txt) {
+    let out = el.querySelector(".js-console");
+    if (out)
+        out.innerHTML = txt;
+}
+
+function getMoves(board, index) {
+    let { row, col } = i2rc(index);
+    let r1 = 3 * (row / 3 | 0);
+    let c1 = 3 * (col / 3 | 0);
+    let moves = 0;
+    for (let r = r1, i = 0; r < r1 + 3; r++) {
+        for (let c = c1; c < c1 + 3; c++, i++) {
+            moves |= board[rc2i(r, c)]
+                | board[rc2i(row, i)]
+                | board[rc2i(i, col)];
+        }
+    }
+    return moves ^ 511;
+}
+
+function unique(allowed, index, value) {
+    let { row, col } = i2rc(index);
+    let r1 = 3 * (row / 3 | 0);
+    let c1 = 3 * (col / 3 | 0);
+    let ir = 9 * row;
+    let ic = col;
+    let uniq_row = true, uniq_col = true, uniq_3x3 = true;
+    for (let r = r1; r < r1 + 3; ++r) {
+        for (let c = c1; c < c1 + 3; ++c, ++ir, ic += 9) {
+            if (uniq_3x3) {
+                let i = rc2i(r, c);
+                if (i != index && allowed[i] & value) uniq_3x3 = false;
+            }
+            if (uniq_row) {
+                if (ir != index && allowed[ir] & value) uniq_row = false;
+            }
+            if (uniq_col) {
+                if (ic != index && allowed[ic] & value) uniq_col = false;
+            }
+            if (!(uniq_3x3 || uniq_row || uniq_col)) return false;
+        }
+    }
+    return uniq_row || uniq_col || uniq_3x3;
+}
+
+function analyze(board) {
+    let allowed = board.map((x, i) => x ? 0 : getMoves(board, i));
+    let bestIndex, bestLen = 100;
+    for (let i = 0; i < 81; i++) if (!board[i]) {
+        let moves = allowed[i];
+        let len = 0;
+        for (let m = 1; moves; m <<= 1) if (moves & m) {
+            ++len;
+            if (unique(allowed, i, m)) {
+                allowed[i] = m;
+                len = 1;
+                break;
+            }
+            moves ^= m;
+        }
+        if (len < bestLen) {
+            bestLen = len;
+            bestIndex = i;
+            if (!bestLen) break;
+        }
+    }
+    return {
+        index: bestIndex,
+        moves: allowed[bestIndex],
+        len: bestLen,
+        allowed: allowed
+    };
 }
